6f817003ec0f0c3c1313d93e0fd655f4c3ed9c4b
alexmerren <alexandermerren@gmail.com>
2023-08-25 12:32:18 +0100

    *

--- cmd/leaderboards-data/main.go
+++ cmd/leaderboards-data/main.go
@@ -1,41 +1,66 @@
 package main
 
 import (
-	"errors"
-	"os"
+	"bufio"
+	"fmt"
+
+	"github.com/alexmerren/speedruncom-scraper/internal/filesystem"
+)
+
+const (
+	allGameIDListV1             = "./data/v1/games-id-list.csv"
+	leaderboardOutputFilenameV1 = "./data/v1/leaderboard-data.csv"
+
+	allGameIDListV2             = "./data/v2/games-id-list.csv"
+	leaderboardOutputFilenameV2 = "./data/v2/leaderboard-data.csv"
 )
 
 func main() {
-	getLeaderboardDataV2()
+	//getLeaderboardDataV1()
 }
 
-func getLeaderboardDataV1() {}
+func getLeaderboardDataV1() {
+	inputFile, err := filesystem.OpenInputFile(allGameIDListV1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer inputFile.Close()
+
+	leaderboardOuptutFile, err := filesystem.CreateOutputFile(leaderboardOutputFilenameV1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer leaderboardOuptutFile.Close()
+	leaderboardOuptutFile.WriteString("#ID,name,URL,type,rules,releaseDate,addedDate,runCount,playerCount,numCategories,numLevels,emulator\n")
+
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Scan()
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
 
 func getLeaderboardDataV2() {
-	// Deal with this stuff in leaderboards-data! \/
-	// what should I do in terms of the breakdown of each number of runs to the game,category,variable,value combination?
-	// What should I do for the values/variable for each game?
-}
-
-func openInputFile(filename string) (*os.File, error) {
-	file, err := os.Open(filename)
+	inputFile, err := filesystem.OpenInputFile(allGameIDListV2)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return
 	}
+	defer inputFile.Close()
 
-	return file, err
-}
-
-func createOutputFile(filename string) (*os.File, error) {
-	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
-		if _, err := os.Create(filename); err != nil {
-			return nil, err
-		}
-	}
-
-	outputFile, err := os.OpenFile(filename, os.O_RDWR, 0)
+	leaderboardOuptutFile, err := filesystem.CreateOutputFile(leaderboardOutputFilenameV2)
 	if err != nil {
-		return nil, err
+		fmt.Println(err)
+		return
 	}
-	return outputFile, nil
+	defer leaderboardOuptutFile.Close()
+	leaderboardOuptutFile.WriteString("")
+
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Scan()
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
 }
